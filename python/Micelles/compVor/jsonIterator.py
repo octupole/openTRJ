979baaf80c53123c039e826328ca23d67c0265da
Massimo Marchi <mm148881@gmail.com>
2018-06-05 18:08:21 +0200

    Changed some python voronoi code

--- python/Micelles/compVor/jsonIterator.py
+++ python/Micelles/compVor/jsonIterator.py
@@ -4,6 +4,8 @@ Created on Dec 17, 2017
 @author: marchi
 '''
 import sys 
+import io
+import json
 
 class JSONIterator(object):
     '''
@@ -23,34 +25,45 @@ class JSONIterator(object):
         if end_ != None:
             self.End=end_
         self.root=root
-        List=[L for L in root if L not in JSONIterator.Keywords ]
-        self.List=[L for L in List if float(L) >= self.Start and float(L) <= self.End]
-        myStart=List[0]
-        myEnd=List[len(List)-1]
-        if not len(self.List):
-            print('Trajectory bounds wrong: Found %-s -- %-s, while given %10.3f -- %10.3f '%(myStart,myEnd,self.Start,self.End))
-            sys.exit(1)
-        if self.Start < float(myStart):
-            self.Start=float(myStart)
-        if self.End > float(myEnd):
-            self.End=float(myEnd)
+
+        if isinstance(root,dict):
+            List=[L for L in root if L not in JSONIterator.Keywords ]
+            self.List=[L for L in List if float(L) >= self.Start and float(L) <= self.End]
+            myStart=List[0]
+            myEnd=List[len(List)-1]
+            if not len(self.List):
+                print('Trajectory bounds wrong: Found %-s -- %-s, while given %10.3f -- %10.3f '%(myStart,myEnd,self.Start,self.End))
+                sys.exit(1)
+            if self.Start < float(myStart):
+                self.Start=float(myStart)
+            if self.End > float(myEnd):
+                self.End=float(myEnd)
             
-        self.iterate=iter(self.List)
-        self.n=0
-        self.end=len(self.List)
-        if end_ != None:
-            print('   Iterating from step %-11.2f through %-11.2f ' %(self.Start,self.End))
-        if start_ == None and end_ == None:
-            print('   Iterating over the entire trajectory from step %-11.2f through %-11.2f ' %(self.Start,self.End))
-            
+            self.iterate=iter(self.List)
+            self.n=0
+            self.end=len(self.List)
+            if end_ != None:
+                print('   Iterating from step %-11.2f through %-11.2f ' %(self.Start,self.End))
+            if start_ == None and end_ == None:
+                print('   Iterating over the entire trajectory from step %-11.2f through %-11.2f ' %(self.Start,self.End))
+        elif isinstance(root,io.TextIOWrapper):
+            while True:
+                self.iterate=json.loads(root.readline())
+                if float(next(iter(self.iterate))) >= self.Start:
+                    break
+
     def __iter__(self):
         return self
     
     def next(self):
-        try:
-            return next(self.iterate)
-        except StopIteration:
-            return None
+        if isinstance(self.root,dict):
+            try:
+                return next(self.iterate)
+            except StopIteration:
+                return None
+        else:
+            self.iterate=json.loads(self.root.readline)
+            return self.iterate
 #             
 #         print(self.n,self.end)
 #         if self.n < self.end:
